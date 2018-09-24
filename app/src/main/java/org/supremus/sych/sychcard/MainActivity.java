aa7c1e8deb39dd4d51092fec6b48d8973d7efd40
sychyow <sychyow@outlook.com>
2018-09-25 00:32:48 +0300

    copyright string generation

--- app/src/main/java/org/supremus/sych/sychcard/MainActivity.java
+++ app/src/main/java/org/supremus/sych/sychcard/MainActivity.java
@@ -1,7 +1,13 @@
 package org.supremus.sych.sychcard;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView cp = new TextView(this);
+        cp.setText("© Dmitri Sychyow, 2018");
+        cp.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
+        cp.setTextColor(ContextCompat.getColor(this, R.color.colorAccent));
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.gravity = Gravity.CENTER_HORIZONTAL;
+        cp.setLayoutParams(lp);
+
+        LinearLayout root = findViewById(R.id.ll_root);
+        root.addView(cp);
     }
 }
