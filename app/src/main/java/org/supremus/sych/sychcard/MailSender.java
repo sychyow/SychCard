520f8488b1ee621b8b3c8a4fc598e7db8b9684a1
sychyow <sychyow@outlook.com>
2018-09-25 09:59:59 +0300

    Mail handling added

---
+++ app/src/main/java/org/supremus/sych/sychcard/MailSender.java
@@ -0,0 +1,29 @@
+package org.supremus.sych.sychcard;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+class MailSender {
+    private final Intent mailIntent;
+    private ComponentName mailName = null;
+
+    MailSender(Context context) {
+        mailIntent = new Intent(Intent.ACTION_SENDTO);
+        mailIntent.setType("text/plain");
+        mailIntent.setData(Uri.parse(String.format("mailto:%s", context.getString(R.string.mail_recipient))));
+        mailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {context.getString(R.string.mail_recipient)});
+        mailIntent.putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.mail_subject));
+        mailName = mailIntent.resolveActivity(context.getPackageManager());
+    }
+
+    public boolean hasActivity() {
+        return mailName != null;
+    }
+
+    public Intent getSendIntent(String text) {
+        mailIntent.putExtra(Intent.EXTRA_TEXT, text);
+        return mailIntent;
+    }
+}
--- app/src/main/java/org/supremus/sych/sychcard/MainActivity.java
+++ app/src/main/java/org/supremus/sych/sychcard/MainActivity.java
@@ -1,16 +1,22 @@
 package org.supremus.sych.sychcard;
 
+import android.content.Intent;
 import android.graphics.Color;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MailSender ms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
 
         LinearLayout root = findViewById(R.id.ll_root);
         root.addView(cp);
+
+        ms = new MailSender(this);
+        Button btnSend = findViewById(R.id.btn_send);
+        btnSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!ms.hasActivity()) {
+            Snackbar.make(v,"Mail app not available", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null).show();
+            return;
+        }
+        EditText etMessage = findViewById(R.id.et_message);
+        Intent mailIntent = ms.getSendIntent(etMessage.getText().toString());
+        startActivity(mailIntent);
     }
 }
