d9f983c987fec62dad81f88391e0423b5dc1ab62
sychyow <sychyow@outlook.com>
2018-10-12 13:26:30 +0300

    Background load for data

--- app/src/main/java/org/supremus/sych/sychnews/NewsListActivity.java
+++ app/src/main/java/org/supremus/sych/sychnews/NewsListActivity.java
@@ -7,13 +7,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.res.Configuration;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import java.lang.ref.WeakReference;
 
 
 public class NewsListActivity extends AppCompatActivity {
+
+    private RecyclerView rv;
+    private ProgressBar pb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,21 +29,20 @@ public class NewsListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news_list);
         Toolbar tb = findViewById(R.id.sych_toolbar);
         setSupportActionBar(tb);
-        RecyclerView rv = findViewById(R.id.rv_root);
+        rv = findViewById(R.id.rv_root);
+        pb = findViewById(R.id.progressbar);
         int orientation = getResources().getConfiguration().orientation;
         if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             LinearLayoutManager llm = new LinearLayoutManager(this);
-            rv.setLayoutManager(llm);
+            getRv().setLayoutManager(llm);
         } else {
             DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
             float dhWidth = displayMetrics.widthPixels / displayMetrics.density;
             int colNum = (int) Math.floor(dhWidth / 300.0);
             GridLayoutManager glm = new GridLayoutManager(this, colNum);
-            rv.setLayoutManager(glm);
+            getRv().setLayoutManager(glm);
         }
-        NewsAdapter na = new NewsAdapter();
-        rv.setAdapter(na);
-        rv.setHasFixedSize(true);
+        new LoadDataTask(this).execute();
     }
 
     @Override
@@ -53,4 +60,76 @@ public class NewsListActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public RecyclerView getRv() {
+        return rv;
+    }
+
+    public PBOff getPBOff(int mode) {
+        return new PBOff(mode);
+    }
+
+    private class PBOff implements Runnable {
+        private final static int MODE_PB = 0;
+        private final static int MODE_RV = 1;
+        private int mode;
+
+        private NewsAdapter na;
+
+        PBOff(int m) {
+            mode = m;
+        }
+
+        void setNewsAdapter(NewsAdapter n) {
+            na = n;
+        }
+
+        @Override
+        public void run() {
+            switch(mode) {
+                case MODE_PB:
+                    pb.setVisibility(View.GONE);
+                    break;
+                case MODE_RV:
+                    getRv().setAdapter(na);
+                    getRv().setHasFixedSize(true);
+                    break;
+            }
+
+        }
+    }
+
+    private static class LoadDataTask extends AsyncTask<Object, Void, Void> {
+        private WeakReference<NewsListActivity> nla;
+
+         LoadDataTask(NewsListActivity activity) {
+            this.nla = new WeakReference<>(activity);
+        }
+        @Override
+        protected Void doInBackground(Object[] objects) {
+            NewsAdapter na = new NewsAdapter();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            NewsListActivity activity = nla.get();
+            if (activity!=null) {
+                PBOff runner = activity.getPBOff(PBOff.MODE_RV);
+                runner.setNewsAdapter(na);
+                activity.runOnUiThread(runner);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void res) {
+            super.onPostExecute(res);
+            NewsListActivity activity = nla.get();
+            if (activity!=null) {
+                activity.runOnUiThread(activity.getPBOff(PBOff.MODE_PB));
+            }
+        }
+    }
+
 }
